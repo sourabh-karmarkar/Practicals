00a27f0ccaff055e7331f61c4d54fd3fb2a6d023
sourabh-karmarkar <sourabh.karmarkar@synerzip.com>
2019-01-14 23:39:56 +0530

    Complete React Tutorial Till Lesson 21

--- SourabhPractice/ReduxPractice/react_redux_tutorial/myapp/src/App.js
+++ SourabhPractice/ReduxPractice/react_redux_tutorial/myapp/src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import Ninjas from './Ninjas';
+import AddNinja from './AddNinja';
 
 class App extends Component {
   state={
@@ -9,12 +10,32 @@ class App extends Component {
       {name:"Akash",age:25,belt:"yellow",id:3},
     ]
   }
+
+  addNinja=(ninja)=>{
+    ninja.id=Math.random();
+    let ninjas=[...this.state.ninjas,ninja];
+    this.setState({
+      ninjas:ninjas
+    });
+    console.log(ninja);
+  }
+
+  deleteNinja=(id)=>{
+    let ninjas=this.state.ninjas.filter(ninja=>{
+      return ninja.id!==id;
+    });
+    this.setState({
+      ninjas:ninjas
+    });
+  }
+
   render() {
     return (
       <div className="App">
         <h1>My First React App</h1>
         <p>Welcome</p>
-        <Ninjas ninjas={this.state.ninjas}/>
+        <Ninjas deleteNinja={this.deleteNinja} ninjas={this.state.ninjas}/>
+        <AddNinja addNinja={this.addNinja}/>
       </div>
     );
   }
