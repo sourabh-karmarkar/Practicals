f7b6c6c96037db43b09a95677aced308678bde1f
sourabh-karmarkar <sourabh.karmarkar@synerzip.com>
2019-01-15 09:24:39 +0530

    TDD Kata 15JAN2019

---
+++ TDD Kata/15JAN2019/PrimeFactor.test.js
@@ -0,0 +1,24 @@
+var primeFactorObject=require('./PrimeFactor');
+describe('Prime Factor Test Cases', () => {
+    test('Input : 1 ---> Output : null', () => {
+        expect(primeFactorObject.primeFactorFunction(1)).toBe(null);
+    });
+    test('Input : 2 ---> Output : 2', () => {
+        expect(primeFactorObject.primeFactorFunction(2)).toEqual([2]);
+    });
+    test('Input : 3 ---> Output : 3', () => {
+        expect(primeFactorObject.primeFactorFunction(3)).toEqual([3]);
+    });
+    test('Input : 4 ---> Output : 2,2', () => {
+        expect(primeFactorObject.primeFactorFunction(4)).toEqual([2,2]);
+    });
+    test('Input : 8 ---> Output : 2,2,2', () => {
+        expect(primeFactorObject.primeFactorFunction(8)).toEqual([2,2,2]);
+    });
+    test('Input : 9 ---> Output : 3,3', () => {
+        expect(primeFactorObject.primeFactorFunction(9)).toEqual([3,3]);
+    });
+    test('Input : 5467384 ---> Output : 2,2,2,13,52571', () => {
+        expect(primeFactorObject.primeFactorFunction(5467384)).toEqual([2,2,2,13,52571]);
+    });
+});
