9030349fbc3b4c37e6b1be96b0d5c28675d10134
sourabh-karmarkar <sourabh.karmarkar@synerzip.com>
2019-01-22 09:29:04 +0530

    TDD Kata 22JAN2019

--- SourabhPractice/Python Training/Day-4/Exercise-2/question_all.py
+++ SourabhPractice/Python Training/Day-4/Exercise-2/question_all.py
@@ -16,7 +16,6 @@ print("%f Fahrenheit = %f Celsius"%(T_fahrenheit,T_celsius))
 """
 	2) What would make this program crash? What would we need to do to handle this situation gracefully?
 	
-	A) If the value entered by the user is not floating point then the program may crash. To handle this we
-	   will have to check whether the value entered by the user is a floating point or not. We will have to handle
-	   the error when the input is being converted to float. If it is float then proceed else give an appropriate 		   message
+	A) If the value entered by the user is not floating point then the program may crash. 
+	   To handle this we will have to check whether the value entered by the user is a floating point or not. We 		   will have to handle the error when the input is being converted to float. If it is float then proceed else 		   give an appropriate message.
 """
---
+++ TDD Kata/22JAN2019/PrimeFactor.test.js
@@ -0,0 +1,25 @@
+var primeFactorObj=require('./PrimeFactor')
+
+describe('Prime Factors Test Cases', () => {
+    test('Input : 1 ---> Output : null', () => {
+        expect(primeFactorObj.primeFactorFunction(1)).toBe(null);
+    });
+    test('Input : 2 ---> Output : 2', () => {
+        expect(primeFactorObj.primeFactorFunction(2)).toEqual([2]);
+    });
+    test('Input : 3 ---> Output : 3', () => {
+        expect(primeFactorObj.primeFactorFunction(3)).toEqual([3]);
+    });
+    test('Input : 4 ---> Output : 2,2', () => {
+        expect(primeFactorObj.primeFactorFunction(4)).toEqual([2,2]);
+    });
+    test('Input : 8 ---> Output : 2,2,2', () => {
+        expect(primeFactorObj.primeFactorFunction(8)).toEqual([2,2,2]);
+    });
+    test('Input : 9 ---> Output : 3,3', () => {
+        expect(primeFactorObj.primeFactorFunction(9)).toEqual([3,3]);
+    });
+    test('Input : 435644 ---> Output : 2,2,11,9901', () => {
+        expect(primeFactorObj.primeFactorFunction(435644)).toEqual([2,2,11,9901]);
+    });
+});
