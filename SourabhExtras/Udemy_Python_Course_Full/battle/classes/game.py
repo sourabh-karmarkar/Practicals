3dff005ff83e7ce76ebc674b3e98750dee8514c5
sourabh-karmarkar <sourabh.karmarkar@synerzip.com>
2019-02-12 09:18:19 +0530

    Python RPG Battle and TDD Kata 12FEB2019

--- SourabhExtras/Udemy_Python_Course_Full/battle/classes/game.py
+++ SourabhExtras/Udemy_Python_Course_Full/battle/classes/game.py
@@ -2,14 +2,14 @@ import random
 
 
 class bcolors:
-    HEADER = '\033[95n'
-    OKBLUE = '\033[94n'
-    OKGREEN = '\033[92n'
-    WARNING = '\033[93n'
-    FAIL = '\033[91n'
-    ENDC = '\033[0n'
-    BOLD = '\033[1n'
-    UNDERLINE = '\033[4n'
+    HEADER = "\033[95m"
+    OKBLUE = "\033[94m"
+    OKGREEN = "\033[92m"
+    WARNING = "\033[93m"
+    FAIL = "\033[91m"
+    ENDC = "\033[0m"
+    BOLD = "\033[1m"
+    UNDERLINE = '\033[4m'
 
 
 class Person:
@@ -37,3 +37,36 @@ class Person:
         if self.hp < 0:
             self.hp = 0
         return self.hp
+
+    def get_hp(self):
+        return self.hp
+
+    def get_max_hp(self):
+        return self.maxhp
+
+    def get_mp(self):
+        return self.mp
+
+    def get_max_mp(self):
+        return self.maxmp
+
+    def reduce_mp(self, cost):
+        self.mp -= cost
+
+    def get_spell_name(self, i):
+        return self.magic[i]["name"]
+
+    def get_spell_mp_cost(self, i):
+        return self.magic[i]["cost"]
+
+    def choose_action(self):
+        i = 1
+        for item in self.actions:
+            print(str(i) + "i", item)
+            i += 1
+
+    def choose_magic(self):
+        i = 1
+        for spell in self.magic:
+            print(str(i) + "i", spell["name"],"(cost:", str(spell["mp"]) + ")")
+            i += 1
--- SourabhExtras/Udemy_Python_Course_Full/battle/main.py
+++ SourabhExtras/Udemy_Python_Course_Full/battle/main.py
@@ -1,12 +1,59 @@
 from classes.game import Person, bcolors
 
 
-magic = [{"name": "Fire", "cost": 10, "dmg": 60},
-         {"name": "Thunder", "cost": 10, "dmg": 80},
-         {"name": "Blizzard", "cost": 10, "dmg": 60}]
+magic = [{"name": "Fire", "cost": 10, "dmg": 100},
+         {"name": "Thunder", "cost": 10, "dmg": 124},
+         {"name": "Blizzard", "cost": 10, "dmg": 100}]
 
 
 player = Person(460, 65, 60, 34, magic)
+enemy = Person(1200, 65, 45, 25, magic)
 
 print(player.generate_spell_damage(0))
 print(player.generate_spell_damage(1))
+
+running = True
+i = 0
+
+print(bcolors.FAIL + bcolors.BOLD + "AN ENEMY ATTACKS!" + bcolors.ENDC)
+
+while running:
+    print("================")
+    player.choose_action()
+    choice = input("Choose action : ")
+    index = int(choice) - 1
+
+    if index == 0:
+        dmg = player.generate_damage()
+        enemy.take_damage(dmg)
+        print("You attacked for", dmg, "points of damage.")
+    elif index == 1:
+        player.choose_magic()
+        magic_choice = int(input("Choose magic : "))
+        magic_dmg = player.generate_spell_damage(magic_choice)
+        spell = player.get_spell_name(magic_choice)
+        cost = player.get_spell_mp_cost(magic_choice)
+
+        current_mp = player.get_mp()
+
+        if cost > current_mp:
+            print(bcolors.FAIL + "\nNot enough MP\n" + bcolors.ENDC)
+            continue
+
+        player.reduce_mp(cost)
+        enemy.take_damage(magic_dmg)
+        print(bcolors.OKBLUE + "\n" + spell + "deals", str(magic_dmg), "points of damage" + bcolors.ENDC)
+
+
+    enemy_choice = 1
+
+    enemy_dmg = enemy.generate_damage()
+    player.take_damage(enemy_dmg)
+    print("Enemy attacks for", enemy_dmg, "Player HP", player.get_hp())
+
+    if enemy.get_hp() == 0:
+        print(bcolors.OKGREEN + "You win!" + bcolors.ENDC)
+        running = False
+    elif player.get_hp() == 0:
+        print(bcolors.FAIL + "Your enemy has defeated you!!" + bcolors.ENDC)
+        running = False
