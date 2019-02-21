7234acd6374dc03ae70146d706e1d20091d2b46e
sourabh-karmarkar <sourabh.karmarkar@synerzip.com>
2019-02-21 17:00:22 +0530

    Deleted All package-lock.json, TDD Kata, RubyOnRails ppt added

---
+++ TDD Kata/21FEB2019/PrimeFactor.test.js
@@ -0,0 +1,25 @@
+primeFactorObj = require('./PrimeFactor')
+
+describe('Prime Factors Test Cases', () => {
+    test('Input : 1 ---> Output : null', () => {
+        expect(primeFactorObj.primeFactorFunction(1)).toBe(null)
+    });
+    test('Input : 2 ---> Output : 2', () => {
+        expect(primeFactorObj.primeFactorFunction(2)).toEqual([2])
+    });
+    test('Input : 3 ---> Output : 3', () => {
+        expect(primeFactorObj.primeFactorFunction(3)).toEqual([3])
+    });
+    test('Input : 4 ---> Output : 2,2', () => {
+        expect(primeFactorObj.primeFactorFunction(4)).toEqual([2,2])
+    });
+    test('Input : 8 ---> Output : 2,2,2', () => {
+        expect(primeFactorObj.primeFactorFunction(8)).toEqual([2,2,2])
+    });
+    test('Input : 9 ---> Output : 3,3', () => {
+        expect(primeFactorObj.primeFactorFunction(9)).toEqual([3,3])
+    });
+    test('Input : 678368 ---> Output : 2,2,2,2,2,17,29,43', () => {
+        expect(primeFactorObj.primeFactorFunction(678368)).toEqual([2,2,2,2,2,17,29,43])
+    });
+});
