c518a38659301001f1d95cfcdf2fed0e239101ba
sourabh-karmarkar <sourabh.karmarkar@synerzip.com>
2019-02-25 23:43:06 +0530

    React Router Tutorial

---
+++ SourabhPractice/ReactJS/react_router_hamza_mirza/README.md
@@ -0,0 +1,3 @@
+React Router Tutorial By Hamza Mirza
+
+https://www.youtube.com/watch?v=91F8reC8kvo
---
+++ SourabhPractice/ReactJS/react_router_hamza_mirza/router/src/components/Error.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Error = () => {
+    return (
+        <div>
+            <p>Path Does Not Exist</p>
+        </div>
+    );
+}
+
+export default Error;
