f9ab9590c255f4fe2444d75b5703d0d7ffcfe39a
sourabh-karmarkar <sourabh.karmarkar@synerzip.com>
2019-03-12 11:45:01 +0530

    CucumberJS 10 minute tutorial

---
+++ SourabhPractice/CucumberJS_Practice/hellocucumber/features/step_definitions/stepdefs.js
@@ -0,0 +1,31 @@
+const assert = require('assert');
+const { Given, When, Then } = require('cucumber');
+
+function isItFriday(today)
+{
+	if (today === "Friday")
+	{
+		return "TGIF"; 
+	}
+	else
+	{
+		return "Nope";
+	}
+}
+
+Given('today is Sunday', function () {
+	this.today = 'Sunday';
+});
+
+Given('today is Friday', function () {
+	this.today = 'Friday';
+});
+
+When('I ask whether it\'s Friday yet', function () {
+	this.actualAnswer = isItFriday(this.today);
+});
+
+Then('I should be told {string}', function (expectedAnswer) {
+	assert.equal(this.actualAnswer, expectedAnswer);
+});
+
